7b1424611512a750760a110179d6d516501e9854
bevexed <547551650@qq.com>
2019-05-28 10:25:36 +0800

    售后 查询

--- miniprogram/sale-after/sale-after.ts
+++ miniprogram/sale-after/sale-after.ts
@@ -2,10 +2,23 @@ import {reqAfterQuery} from "../api/index";
 
 Page({
 	data: {
-		imei: ''
+		imei: '',
+
+		iCode: 0,
+
+		code: 0,
 	},
 
 	onLoad(): void {
+		this.getCode()
+	},
+
+	getCode() {
+		let code = (Math.random() * 1000000).toString().slice(0, 4);
+		console.log(code);
+		this.setData!({
+			code
+		})
 	},
 
 	IMEI(e: any) {
@@ -15,8 +28,24 @@ Page({
 		})
 	},
 
+	icode(e: any) {
+		console.log(this.data.imei);
+		this.setData!({
+			iCode: e.detail.value
+		})
+	},
+
 	getAfterQuery() {
-		const {imei} = this.data;
+		const {imei, iCode, code} = this.data;
+		if (iCode !== code) {
+			wx.showToast({
+				mask: true,
+				icon: "none",
+				title: '验证码错误',
+				duration: 2000,
+			});
+			return
+		}
 		reqAfterQuery(imei).then(
 			res => {
 				console.log(res);
